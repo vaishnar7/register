a546f045592dba220b4d5bc2b6e99d6a144a8362
Sibin <sibin.t@urolime.com>
2019-12-26 12:32:13 +0530

    added reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { TodoComponent } from './todo/todo.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-todo',
@@ -8,13 +9,42 @@ import { Component, OnInit } from '@angular/core';
 export class TodoComponent implements OnInit {
   title="Todo 1";
   todos=[
-    "todo 1",
-    "todo 2",
-    "todo 3",
-  ]
-  constructor() { }
+    { name:"todo 1", description:"description1" },
+    { name:"todo 2", description:"description2" },
+    { name:"todo 3", description:"description3" },
+  ];
+  newTodo="";
+  todoForm;
+
+  tempValue='';
+
+  constructor(private fb: FormBuilder) {
+    this.todoForm = fb.group({
+      name: '',
+      description: ''
+    });
+  }
 
   ngOnInit() {
   }
 
+  delete(todo){
+    this.todos=this.todos.filter(function(t){
+      return t!=todo;
+    })
+  }
+  add(){
+    this.todos.push({
+      name: this.todoForm.value.name,
+      description: this.todoForm.value.description,
+    });
+    // this.newTodo="";
+  }
+
+  onTextChange(event){
+    this.newTodo=event.target.value;
+  }
+  onChange($event){
+    this.tempValue = $event.target.value;
+  }
 }
